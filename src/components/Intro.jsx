e0acb6cac4eeac59de454accc073c626748cbfa9
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-08-27 14:20:17 -0500

    re-ordered content in intro class

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,12 +3,21 @@ import React from "react";
 const Intro = () => {
   return (
     <div className="intro">
-      <div className="headertext">
-        <h1>I'm Keith</h1>
-        <h3>
-          A Full Stack Developer specializing in React, .Net Core, and Redux.
-        </h3>
-        <br />
+      <div className="intro-content">
+        <h1 className="intro-title">
+          I'm Keith. A Full Stack Developer specializing in React, and .Net
+          Core.
+        </h1>
+      </div>
+      <div className="experience">
+        <h2 className="experience-title">Years of Experience:</h2>
+        <ul className="experience-list">
+          <li>HTML: 3</li>
+          <li>CSS: 3</li>
+          <li>SQL: 3</li>
+          <li>React: 2</li>
+          <li>.Net: 1</li>
+        </ul>
       </div>
     </div>
   );
