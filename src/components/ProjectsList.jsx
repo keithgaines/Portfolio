534320faa265972e55feaada6860d0f2ac693205
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-08-27 13:33:42 -0500

    added basic styling

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import Project from "./Project";
 
 const projectsData = [
-  // ... Your projects data here
+  {
+    title: "To Do List",
+    imageSrc: "images/todolist.png",
+    githubLink: "https://github.com/keithgaines/todolist_react",
+    liveSiteLink: "https://todolisttsx.vercel.app",
+  },
+  {
+    title: "Modern Art Gallery",
+    imageSrc: "images/artgallery.png",
+    githubLink: "https://github.com/keithgaines/art-gallery-website",
+    liveSiteLink: "https://keithgaines.github.io/art-gallery-website",
+  },
+  {
+    title: "Galleria Slideshow",
+    imageSrc: "images/galleria.png",
+    githubLink: "https://github.com/keithgaines/slideshowreact",
+    liveSiteLink: "https://slideshowreact.vercel.app/",
+  },
+  {
+    title: "Photosnap Stories",
+    imageSrc: "images/photosnap.png",
+    githubLink: "https://github.com/keithgaines/photosnapFullStack",
+    liveSiteLink: "https://photosnapchallenge.vercel.app/",
+  },
 ];
 
 const ProjectsList = () => (
@@ -11,9 +34,9 @@ const ProjectsList = () => (
       <Project
         key={project.title}
         title={project.title}
-        screenshot={project.screenshot}
-        codeLink={project.codeLink}
-        liveLink={project.liveLink}
+        screenshot={project.imageSrc}
+        codeLink={project.githubLink}
+        liveLink={project.liveSiteLink}
       />
     ))}
   </div>
