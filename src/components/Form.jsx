525654a9a573a5407b6fe5eb4f9cb33bcbb835b3
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-08-27 13:35:29 -0500

    added labels to inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,6 +10,7 @@ function ContactForm() {
       <label htmlFor="email">Email Address</label>
       <input id="email" type="email" name="email" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <label htmlFor="message">Your message</label>
       <textarea id="message" name="message" />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button type="submit" disabled={state.submitting}>
