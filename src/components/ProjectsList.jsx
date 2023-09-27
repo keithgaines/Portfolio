19d3d842d25f16f3544951ad763f6d7fb2417f9e
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-09-27 14:12:39 -0500

    added Kanban to portfolio

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -3,25 +3,32 @@ import Project from "./Project";
 
 const projectsData = [
   {
+    title: "Kanban Web App",
+    imageSrc: "../images/kanban.png",
+    githubLink: "https://github.com/keithgaines/kanban_redux",
+    liveSiteLink: "https://kanban-redux.vercel.app/",
+    technologiesUsed: "React, Redux, Tailwind CSS",
+  },
+  {
     title: "To Do List",
     imageSrc: "../images/todolist.png",
     githubLink: "https://github.com/keithgaines/todolist_react",
     liveSiteLink: "https://todolisttsx.vercel.app",
-    technologiesUsed: "HTML, CSS, React, LocalStorage",
+    technologiesUsed: "React, LocalStorage",
   },
   {
     title: "Modern Art Gallery",
     imageSrc: "../images/artGallery.png",
     githubLink: "https://github.com/keithgaines/art-gallery-website",
     liveSiteLink: "https://keithgaines.github.io/art-gallery-website",
-    technologiesUsed: "HTML, CSS, React, Bootstrap",
+    technologiesUsed: "React, Bootstrap",
   },
   {
     title: "Galleria Slideshow",
     imageSrc: "../images/galleria.png",
     githubLink: "https://github.com/keithgaines/slideshowreact",
     liveSiteLink: "https://slideshowreact.vercel.app/",
-    technologiesUsed: "HTML, CSS, React, Bootstrap",
+    technologiesUsed: "React, Bootstrap",
   },
   {
     title: "Bulky Book",
